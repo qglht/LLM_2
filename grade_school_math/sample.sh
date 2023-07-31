ec673bb5009dc39c84ac275118e31086bd8792dc
qglht <quentin.guilhot@student-cs.fr>
2023-07-31 14:02:35 +0200

    batch script

---
+++ grade_school_math/sample.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=train
+#SBATCH --time 04:00:00
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:16g
+
+module load gcc/8.2.0 python_gpu/3.10.4 eth_proxy
+python sample.py "$@"
